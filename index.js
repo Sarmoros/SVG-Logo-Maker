e610a2c9edcd24d03b8cebc86fc059b056debe6d
Sarahy M <sarahym0703@gmail.com>
2023-12-11 17:58:37 -0500

    added functions for prompts for user input

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const Text = require('./lib/text');
 
 
 
+// maybe how to write the questions for input?
+// should they stay on index.js or should they be moved to another file?
+async function promptForText() {
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the text:',
+        validate: (input) => input.length > 0 && input.length <= 3,
+      },
+    ]);
+    return answers.text;
+}
+
+async function promptForTextColor() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a color keyword or hexadecimal number for the text:',
+        },
+    ]);
+    return answers.textColor;
+}
+
+async function promptForShape() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape for the logo:',
+            choices: Shapes.getSupportedShapes(),
+        },
+    ]);
+    return answers.shape;
+}
+
+async function promptForShapeColor() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter a color keyword or hexadecimal number for the shape color:',
+        },
+    ]);
+    return answers.shapeColor;
+}
+
+// create function to generate the SVG logo
