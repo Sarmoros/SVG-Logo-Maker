10ceafca8b6da4f9e10714d9feff03dd3fbc7eee
Sarahy M <sarahym0703@gmail.com>
2023-12-11 14:25:10 -0500

    added all files and packages, and added a couple lines of code for each file

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Shapes = require('./lib/shapes');
+const Text = require('./lib/text');
+
+
+
+// maybe how to write the questions for input?
+
+
+
